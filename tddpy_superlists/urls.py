bd18eff64a960a4c9eb30bf80540a6a6a8872f24
Richard Placide <richard.placide@gmail.com>
2015-05-20 21:14:04 -0400

    next

--- tddpy_superlists/urls.py
+++ tddpy_superlists/urls.py
@@ -18,8 +18,8 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^$', 'lists.views.home', name='home'),
-    url(r'^lists/the-only-list-in-the-world/$', 'lists.views.view_list',
-            name='view_list'),
+    url(r'^lists/(\d+)/$', 'lists.views.view_list', name='view_list'),
+    url(r'^lists/(\d+)/new_item$', 'lists.views.add_item', name='add_item'),
     url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
     url(r'^admin/', include(admin.site.urls)),
 ]
